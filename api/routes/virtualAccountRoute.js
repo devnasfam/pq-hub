d56307142a1bedab4b71bebbf7c9c5e9274994aa
Dev Nasfam <amajidahmad848@gmail.com>
2024-06-30 11:19:42 +0100

    Delete api directory

--- api/routes/virtualAccountRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { createVirtualAccount } from '../controllers/virtualAccountController.js';
-
-const router = Router();
-
-router.post('/', createVirtualAccount);
-
-export default router;
--- api/routes/webhookRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { handlePaymentNotification } from "../controllers/webhookController.js"; 
-
-const router = Router();
-
-router.post('/payvessel_payment_done', handlePaymentNotification);
-
-export default router;
